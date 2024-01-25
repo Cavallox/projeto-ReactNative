02ea2f951e9e707640ee98d7702438e36d9c58ed
Cavallox <felipe_cavallo@hotmail.com>
2024-01-25 16:49:53 -0300

    criação de components

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,17 +3,17 @@ import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
-        backgroundColor: '#131016',
+        backgroundColor: '#310552',
         padding: 24,
     },
     eventName: {
-      color:'#fdfcfe',
+      color:'#ffffff',
           fontSize: 24,
           fontWeight:  'bold',
           marginTop: 48
     },
     eventDate: {
-      color:'#6b6b6b',
+      color:'#d3d3d3',
       fontSize: 16,
     },
 
